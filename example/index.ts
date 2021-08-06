0b4b5de03e35b7d0dc0858ad0e7b0bf29c5f8d35
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2021-08-06 15:47:52 -0500

    Remove token... again...

--- example/index.ts
+++ example/index.ts
@@ -19,7 +19,7 @@ import { ExampleBot } from './example-bot';
             ],
         },
     });
-    await bot.run('MTg0MTU2NTExMzYxMDQwMzg0.V0KDHQ.6tnRKkZrycanwbOelY6wG0Jvr0U');
+    await bot.run('TOKEN_HERE');
 })().catch(error => {
     console.error(error);
     process.exit(1);
